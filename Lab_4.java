cacdfcc8af74df62c290b65d75bf05e9bab134b6
Vinay0530 <112411494+Vinay0530@users.noreply.github.com>
2022-09-19 16:18:11 -0400

    Add files via upload

---
+++ Lab_4.java
@@ -0,0 +1,54 @@
+package edu.pitt.lab_4;
+
+import javax.swing.JOptionPane;
+
+public class Lab_4 {
+
+	public static void main(String[] args) 
+	{
+		int userNum4 = 0;
+		int userNum6 = 0;
+		
+		String userNum = JOptionPane.showInputDialog("Type your b or base value must be greater than 1.");
+		int userNum1 = Integer.parseInt(userNum);
+		
+		String userNum0 = JOptionPane.showInputDialog("Type your x value must be greater than 0.");
+		int userNum2 = Integer.parseInt(userNum0);
+		
+		if(userNum1 < 1 || userNum2 < 0)
+		{
+			while(userNum1 < 1 || userNum2 < 0)
+			{
+				String userNum3 = JOptionPane.showInputDialog("Retype your b or base value must be greater than 1.");
+				userNum4 = Integer.parseInt(userNum3);
+				
+				String userNum5 = JOptionPane.showInputDialog("Retype your x value must be greater than 0.");
+				userNum6 = Integer.parseInt(userNum5);
+				
+				if(userNum4 > 1 && userNum6 > 0)
+				{
+					double w = logOfBase(userNum4,userNum6);
+			        System.out.println(w);
+			        break;
+				}
+			}
+			
+		
+	      
+	        
+		}
+		
+		else
+		{
+			double k = logOfBase(userNum1,userNum2);
+	        System.out.println(k);
+		}
+		
+		
+	}
+	public static double logOfBase(int baseNumber, int number) 
+	{
+        return Math.log(number) / Math.log(baseNumber);
+    }
+
+}
